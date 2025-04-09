255622f8afafd6920abe5e1662cdb580babd92bc
MartinRangel16 <114451321+MartinRangel16@users.noreply.github.com>
2025-04-08 20:44:04 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,3 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
     apiKey: "AIzaSyBbQ9V1EeAMFqShg_ScqO3ALE2UsGXRSjc",
     authDomain: "encuestashanty12.firebaseapp.com",
@@ -9,10 +7,6 @@ const firebaseConfig = {
     appId: "1:692171791223:web:f1e799d155820ac7389f72",
     measurementId: "G-MF4K1QHYXG"
     };
+// Inicializa Firebase
+firebase.initializeApp(firebaseConfig);
 
-// Inicialización explícita
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-// Exporta los servicios que necesites
-export { db };
