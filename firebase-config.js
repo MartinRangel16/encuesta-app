b9a92f0662c805e8efed5254a529d629fb6abe73
MartinRangel16 <114451321+MartinRangel16@users.noreply.github.com>
2025-04-08 20:12:45 -0600

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBbQ9V1EeAMFqShg_ScqO3ALE2UsGXRSjc",
+    authDomain: "encuestashanty12.firebaseapp.com",
+    projectId: "encuestashanty12",
+    storageBucket: "encuestashanty12.firebasestorage.app",
+    messagingSenderId: "692171791223",
+    appId: "1:692171791223:web:f1e799d155820ac7389f72",
+    measurementId: "G-MF4K1QHYXG"
+    };
